23609138f48e5f0590f762562e45d40f67c96d61
ParkMyungJae <qkraudwo0109@naver.com>
2019-06-01 16:32:53 +0900

    재수정

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <div class="row mt-4 d-flex justify-content-center">
             <div class="col-md-10 col-sm-12">
                 <div class="jumbotron" style="background-image: url('./board.jpg'); background-size: 992px 430px; background-position: center center; background-repeat: no-repeat; color: white;">
-                    <h1 class="display-4 pl-5">오늘의 게시판s</h1>
+                    <h1 class="display-4 pl-5">오늘의 게시판</h1>
                     <p class="lead pl-5">양디고 게시판!! 아무말 대잔치~~ 노래위주 게시판!! `` 강촌사람들 리메이크 모음집 ``</p>
                     <hr class="my-4">
                     <div class="text-right pr-5">
